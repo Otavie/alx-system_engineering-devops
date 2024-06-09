f0e4d86ba4cda95819483967c62e68e39334ce18
Otavie <otavieokuoyo@gmail.com>
2024-06-08 23:52:33 -0700

    Modified the code for 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/Sender: ([^,]+), Receiver: ([^,]+), Flags: \[([^\]]+)\]/).map{ |match| "[#{match[0]}],[#{match[1]}],[#{match[2]}]" }.join
+puts ARGV[0].scan(/\[from:(\+?\d+)\] \[to:(\+?\d+)\] \[flags:([^\]]+)\]/).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }.join
